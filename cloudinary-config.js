6f05307c02eb16293eebfae84a6c203ab0af5422
abidurrahamanjoy <abidurrahmanjoy56@gmail.com>
2026-09-21 15:22:07 +0600

    Fix syntax error in cloudinary-config.js

--- cloudinary-config.js
+++ cloudinary-config.js
@@ -1,6 +1,3 @@
-// Cloudinary browser-upload settings.
-// This uses a restricted unsigned image upload preset.
-// Never put your Cloudinary API Secret here.
 export const cloudinaryConfig = {
   cloudName: "kzrmwfn8",
   uploadPreset: "joy portfolio"
